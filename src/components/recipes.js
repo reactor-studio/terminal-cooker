695aa622b7e717e8ef59cf179c5e9666ccb0361a
Dino Trojak <dinotrojak@gmail.com>
2018-12-01 12:51:12 +0100

    added rendering of recipes

--- src/components/recipes.js
+++ src/components/recipes.js
@@ -1,12 +1,24 @@
 const { h, Component, Color } = require('ink');
+const SelectInput = require('ink-select-input');
+
+const Item = props => {
+	const ingridients = props.usedIngredients.map(i => i.name).join(', ');
+	let missingIngridients = props.missedIngredients.map(i => i.name);
+	if (missingIngridients.length > 5) {
+		missingIngridients = missingIngridients.slice(0, 4).join(', ') + '...';
+	}
+	return (
+		<Color blue={props.isSelected}>
+			{props.title} - [
+			<Color green>{ingridients}</Color>, <Color red>{missingIngridients}</Color>
+			]
+		</Color>
+	);
+};
 
 class Recipes extends Component {
 	render(props) {
-		return (
-			<div>
-				<Color>This are recipes</Color>
-			</div>
-		);
+		return <SelectInput items={props.recipes} itemComponent={Item} />;
 	}
 }
 
